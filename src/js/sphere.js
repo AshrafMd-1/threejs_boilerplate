cb22a6cb689239eb0e1340f585dcf86fe399f855
AshrafMd-1 <21951A6612@iare.ac.in>
2023-01-13 18:07:41 +0530

    change sphere material properties

--- src/js/sphere.js
+++ src/js/sphere.js
@@ -1,12 +1,8 @@
 import * as THREE from 'three';
 
-const textureLoader = new THREE.TextureLoader();
-
 const sphereGeometry = new THREE.SphereGeometry(5, 64, 64);
 const sphereMaterial = new THREE.MeshStandardMaterial({
-    roughness: 1,
-    metalness: 1,
-    normalMap: textureLoader.load('../assets/textures/NormalMap1.jpg'),
+    color: 0x156289,
 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
