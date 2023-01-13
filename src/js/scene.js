919ce333711174f564c5ad59e5fc0e4a429cd192
AshrafMd-1 <21951A6612@iare.ac.in>
2023-01-13 23:38:45 +0530

    Add a loop to iterate through all mesh items

--- src/js/scene.js
+++ src/js/scene.js
@@ -1,11 +1,15 @@
 import * as THREE from 'three';
-import sphere from './sphere.js';
+import mesh from './mesh.js';
 import camera from './camera.js';
 import light from './light.js';
 
 const scene = new THREE.Scene();
 scene.add(camera);
-scene.add(sphere);
 scene.add(light);
+
+const items=Object.keys(mesh);
+for (let i = 0; i < items.length; i++) {
+    scene.add(mesh[items[i]]);
+}
 
 export default scene;
